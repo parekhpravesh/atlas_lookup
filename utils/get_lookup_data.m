2892ccfbe3ce4760b4ab39ab394237aa5a37d1c8
parekhpravesh <praveshparekh@gmail.com>
2017-07-29 18:50:33 +0530

    Added appending "Undefined" and sorting of data

--- utils/get_lookup_data.m
+++ utils/get_lookup_data.m
@@ -1,9 +1,15 @@
-function [lookup_path, lookup_name, lookup_idx, lookup_names] = get_lookup_data(path_to_lookup)
+function [lookup_path, lookup_name, lookup_idx, lookup_names] = ...
+    get_lookup_data(path_to_lookup, append_undef, sort_data)
 % Function that returns information about lookup tables
 %% Input
-% path_to_lookup:       Tab separated text file with full path to lookup
+% path_to_lookup:       tab separated text file with full path to lookup
 %                       table; first column should have indices and
 %                       second column should have names
+% append_undef:         If 1, "0" and "Undefined" are added to lookup_idx
+%                       and lookup_names respectively (after checking to
+%                       ensure that they are not present)
+% sort_data:            If 1, lookup_data is sorted in ascending order and
+%                       lookup_names is sorted accordingly
 %
 %% Outputs
 % lookup_path:          Path to the lookup table
@@ -11,13 +17,47 @@ function [lookup_path, lookup_name, lookup_idx, lookup_names] = get_lookup_data(
 % lookup_idx:           Indices in the lookup table (numeric)
 % lookup_names:         Names in the lookup table   (cell type string)
 %
+%% Notes
+% If append_undef is 1, lookup_idx is internally sorted in ascending order
+% first, before checking if the first entry is defined as "0"; "0" and
+% "Undefined" are always added to the top of the list.
+%
+% The order of lookup_idx and lookup_names is only modified if sort_data is
+% 1; if append_undef is 1 but sort_data is 0, "0" and "Undefined" are added
+% to the top of the list but the rest of the list remains in the original
+% order
+%
+%% Defaults
+% append_undef    =     0
+% sort_data       =     0
+%
 %% Author(s)
 % Parekh, Pravesh
 % July 24, 2017
 % MBIAL
-%
 
-%%
+%% Evaluate inputs
+if nargin < 1
+    error('Insufficient number of inputs');
+else
+    if nargin == 1
+        append_undef = 0;
+        sort_data    = 0;
+    else
+        if nargin == 2
+            sort_data = 0;
+        else
+            if isempty(append_undef)
+                append_undef = 0;
+            end
+            if isempty(sort_data)
+                sort_data = 0;
+            end
+        end
+    end
+end
+
+%% Read and return lookup information
 % Check if lookup table exists
 if ~exist(path_to_lookup, 'file')
     error('Lookup file not found');
@@ -36,6 +76,19 @@ else
             % Assign into output variables
             lookup_idx   = lookup_data{1};
             lookup_names = lookup_data{2};
+            % Checking if append_undef is 1
+            if append_undef == 1
+                tmp_idx_list = sort(lookup_idx);
+                if tmp_idx_list(1) ~= 0
+                    lookup_idx      =   [0;             lookup_idx];
+                    lookup_names    =   [{'Undefined'}; lookup_names];
+                end
+            end
+            % Checking if sort_data is 1
+            if sort_data == 1
+                [lookup_idx, new_order] = sort(lookup_idx);
+                lookup_names            = lookup_names(new_order);
+            end
         else
             error('File is not organized correctly');
         end
