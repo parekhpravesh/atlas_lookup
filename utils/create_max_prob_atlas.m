39a06a2762c56c3baa9fff214729b4adde18ff83
parekhpravesh <praveshparekh@gmail.com>
2017-08-01 21:25:56 +0530

    Added output file name

--- utils/create_max_prob_atlas.m
+++ utils/create_max_prob_atlas.m
@@ -1,9 +1,11 @@
-function create_max_prob_atlas(path_to_atlas, threshold)
+function create_max_prob_atlas(path_to_atlas, threshold, output_file_name)
 % Function to create a maximum probability atlas from a 4D probabilistic
 % map after thresholding
 %% Inputs:
 % path_to_atlas:    full path to 4D probability map(s)
 % threshold:        numeric value to threshold with (optional)
+% output_file_name: file name (optionally: with full path) which will be
+%                   written (optional)
 % 
 %% Output:
 % A 3D NIfTI file with an intensity value corresponding to a particular
@@ -31,6 +33,7 @@ function create_max_prob_atlas(path_to_atlas, threshold)
 % 
 %% Default
 % threshold = 0.25
+% default output file name is atlas_name_maxprob_thr_threshold.nii
 % 
 %% Author(s)
 % Parekh, Pravesh
@@ -45,9 +48,41 @@ if size(atlas_header, 1) == 1
 end
 num_rois = size(atlas_header,1);
 
-% Setting default threshold of 0.25
+% Setting defaults
 if nargin == 1
     threshold = 0.25;
+    output_file_name =  fullfile(atlas_path, [atlas_name, '_maxprob_thr', ...
+                        num2str(threshold), '.nii']);
+    warning_file_name = fullfile(atlas_path, [atlas_name, '_maxprob_thr', ...
+                        num2str(threshold), '_warnings.txt']);
+else
+    if nargin == 2
+        if isempty(threshold)
+            threshold = 0.25;
+        end
+        output_file_name =  fullfile(atlas_path, [atlas_name, '_maxprob_thr', ...
+                            num2str(threshold), '.nii']);
+        warning_file_name = fullfile(atlas_path, [atlas_name, '_maxprob_thr', ...
+                            num2str(threshold), '_warnings.txt']);
+    else
+        if nargin == 3
+            if isempty(threshold)
+                threshold = 0.25;
+            end
+            if isempty(output_file_name)
+                output_file_name  = fullfile(atlas_path, [atlas_name, ...
+                                    '_maxprob_thr', num2str(threshold), '.nii']);
+                warning_file_name = fullfile(atlas_path, [atlas_name, ...
+                                    '_maxprob_thr', num2str(threshold), '_warnings.txt']);
+            else
+                [tmp_path, tmp_name, tmp_ext] = fileparts(output_file_name);
+                if isempty(tmp_path)
+                    output_file_name  = fullfile(atlas_path, [tmp_name, tmp_ext]);
+                    warning_file_name = fullfile(atlas_path, [tmp_name, '_warnings.txt']);
+                end
+            end
+        end
+    end
 end
 
 %% Thresholding
@@ -76,8 +111,7 @@ atlas_data2 = reshape(atlas_data, [size(atlas_xyz,1), num_rois]);
 warn_count = 0;
 
 % Open a file to write warnings
-fname = fullfile(atlas_path, [atlas_name, '_maxprob_thr', num2str(threshold), '_warnings.txt']);
-fid = fopen(fname, 'w');
+fid = fopen(warning_file_name, 'w');
 
 % Check if multiple regions have the same probability (equal to max prob)
 for i = 1:length(val)
@@ -89,7 +123,7 @@ for i = 1:length(val)
                 ' have maximum probability of ', num2str(val(i), '%02d'), ...
                 ' at voxel number ', num2str(i), '; assigning ', ...
                 num2str(idx(i), '%02d'), '; x,y,z : [', ...
-                num2str(atlas_xyz(i,:), '%02d\t '), ']', ];
+                num2str(atlas_xyz(i,:), '%02d '), ']', ];
             fprintf(fid, '%s\r\n', warning_msg);
         end
     end
@@ -108,6 +142,6 @@ atlas_data_mod = reshape(atlas_data_mod, size(squeeze(atlas_data(:,:,:,1))));
 
 %% Modify header and write
 vol_header = atlas_header(1);
-vol_header.fname = fullfile(atlas_path, [atlas_name, '_maxprob_thr', num2str(threshold), '.nii']);
+vol_header.fname = output_file_name;
 vol_header.dt = [4,0];
 spm_write_vol(vol_header, atlas_data_mod);
