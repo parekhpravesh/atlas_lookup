486c37f0a7ee0cd3a120b2364033d80c382edd09
parekhpravesh <praveshparekh@gmail.com>
2017-08-06 14:39:17 +0530

    Edited header; improved file writing; using get_atlas_data

--- utils/merge_rois.m
+++ utils/merge_rois.m
@@ -1,39 +1,46 @@
-function merge_rois(atlas_file, save_dir, roi_idx, vol_name)
-% Function to merge a set of ROIs
-% roi_idx can take the following values:
-% a vector of numbers corresponding to indices in the atlas_file
-% 'L':      left (x<0)
-% 'R':      right (x>0)
-% 'M':      midline (x=0)
-% 'LM':     left+midline (x<=0)
-% 'RM':     right+midline (x>=0)
-% 'LR':     left+midline (x<0 AND x>0 i.e. x~=0)
-% 'LRM':    the entire brain (left+right+midline)
-% vol_name is the filename with which the output volume is written
+function merge_rois(path_to_atlas, roi_idx)
+% Function to merge a set of ROIs into a binary mask
+%% Inputs:
+% path_to_atlas:        full path to atlas file
+% roi_idx:              indices to be merged (see below)
 % 
+%% Output:
+% A binary mask containing "1"s for the coordinates covered by roi_idx If
+% roi_idx is a special case, output file name is suffixed
+% "_bin_merged_<roi_idx>", otherwise is suffixed "bin_merged_idx"
+% 
+%% The following values for roi_idx are supported:
+% a vector of numbers corresponding to indices in the atlas_file
+% 'L':                  left            (x< 0)
+% 'R':                  right           (x> 0)
+% 'M':                  midline         (x= 0)
+% 'LM':                 left+midline    (x<=0)
+% 'RM':                 right+midline   (x>=0)
+% 'LR':                 left+midline    (x<0 AND x>0 i.e. x~=0)
+% 'LRM':                the entire brain (left+right+midline)
+% 
+%% Author(s)
 % Parekh, Pravesh
 % June 24,2017
 % MBIAL
 
-% Load atlas file
-atlas_vol = spm_vol(atlas_file);
-[atlas_data, atlas_xyz] = spm_read_vols(atlas_vol);
-
-% Get number of ROIs defined in the atlas
-list_rois = unique(nonzeros(atlas_data(:)));
+%% Read atlas data, check inputs, and initialize
+[atlas_header, atlas_path, atlas_name, atlas_data, atlas_xyz, ...
+    all_labels, ~] = get_atlas_data(path_to_atlas);
 
 % Check if the provided roi_idx actually exist in the atlas
 if isnumeric(roi_idx)
     num_rois = length(roi_idx);
-    chk = setdiff(roi_idx, list_rois);
+    chk = setdiff(roi_idx, all_labels);
     if ~isempty(chk)
         error('One or more roi_idx input does not exist');
     end
 end
 
+% Initialize
 binary_mask_data = zeros(size(atlas_data));
 
-% Merge regions
+%% Merge regions
 if isnumeric(roi_idx)
     tmp = zeros(size(atlas_data));
     
@@ -48,27 +55,36 @@ if isnumeric(roi_idx)
     for roi = 1:num_rois
         binary_mask_data(atlas_data == roi_idx(roi)) = 1;
     end
+    name = fullfile(atlas_path, [atlas_name, '_bin_merged_idx.nii']);
 else
-    % Get atlas dimensions and then reorganize atlas_data and atlas_xyz
+    % Get atlas dimensions and reorganize atlas_data
     atlas_data_dims = size(atlas_data);
-    atlas_data = atlas_data(:)~=0;
-    atlas_xyz = atlas_xyz(1:3,:)';
+    atlas_data      = atlas_data(:)~=0;
     
     switch(roi_idx)
         case 'L'
             all_x = (atlas_xyz(:,1)<0);
+            name  = fullfile(atlas_path, [atlas_name, '_bin_merged_L.nii']);
         case 'R'
             all_x = (atlas_xyz(:,1)>0);
+            name  = fullfile(atlas_path, [atlas_name, '_bin_merged_R.nii']);
         case 'M'
             all_x = (atlas_xyz(:,1)==0);
+            name  = fullfile(atlas_path, [atlas_name, '_bin_merged_M.nii']);
         case 'LM'
             all_x = (atlas_xyz(:,1)<=0);
+            name  = fullfile(atlas_path, [atlas_name, '_bin_merged_LM.nii']);
         case 'RM'
             all_x = (atlas_xyz(:,1)>=0);
+            name  = fullfile(atlas_path, [atlas_name, '_bin_merged_RM.nii']);
         case 'LR'
             all_x = (atlas_xyz(:,1)~=0);
+            name  = fullfile(atlas_path, [atlas_name, '_bin_merged_LR.nii']);
         case 'LRM'
             all_x = ones(length(atlas_xyz(:,1)),1);
+            name  = fullfile(atlas_path, [atlas_name, '_bin_merged_LRM.nii']);
+        otherwise
+            error('Unable to process roi_idx');
     end
     if ~isempty(all_x)
         binary_mask_data = reshape(atlas_data.*all_x, atlas_data_dims);
@@ -77,9 +93,6 @@ else
     end
 end
 
-binary_mask_header = atlas_vol;
-binary_mask_header.fname = vol_name;
-cd(save_dir);
-
-% Save volume
-spm_write_vol(binary_mask_header, binary_mask_data);
+%% Modify header and write
+atlas_header.fname = name;
+spm_write_vol(atlas_header, binary_mask_data);
